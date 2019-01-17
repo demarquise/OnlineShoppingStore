a060fd386572c228cae2f274feeeccfa0f75fe80
demarquise boone <demarquise_boone@yahoo.com>
2019-01-16 19:41:28 -0500

    added a shirt model an gave it a view.

---
+++ OnlineShoppingStore/Controllers/ShirtController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineShoppingStore.Models;
+
+namespace OnlineShoppingStore.Controllers
+{
+    public class ShirtController : Controller
+    {
+        // GET: Shirt
+        public ActionResult Supremeshirt()
+        {
+            shirts SP = new shirts() { supreme = "Suprme Shirt XL" };
+           
+
+
+            return View(SP);
+        }
+    }
+}
---
+++ OnlineShoppingStore/Models/shirts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShoppingStore.Models
+{
+    public class shirts
+    {
+        
+            public string supreme { get; set; }
+    
+    }
+}
